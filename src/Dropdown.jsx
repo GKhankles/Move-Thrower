e5d18f0bf72ea1a27be344765a68b962203105fd
glightfoot <47872793+lightfootg@users.noreply.github.com>
2021-03-23 18:45:56 -0500

    added Dropdown.jsx
    added a react component for a dropdown menu

---
+++ src/Dropdown.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            uid: ""
+        };
+    }
+
+    onOptionChange(event) {
+        this.setState({
+            option: event.target.value
+        });
+        this.props.getOption(event.target.value);
+    }
+
+    getOptionList(names) {
+        let optionsList = [];
+        names.forEach(element => (
+            optionsList.push(
+                <option name={element}>{element}</option>
+            )
+        ));
+        return optionsList;
+    }
+
+    /*shouldComponentUpdate() {
+        return (this.state.gen)
+    }*/
+
+    componentDidMount() {
+        this.setState({
+            optionsList: this.getOptionList(this.props.names)
+        })
+    }
+
+    render() {
+        let optionsList = this.state.optionsList;
+
+        return (
+            <select value={this.state.option} onChange={this.onOptionChange}>
+                {optionsList}
+            </select>
+        );
+    }
+
+}
