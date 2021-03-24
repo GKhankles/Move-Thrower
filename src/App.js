40e3ce14d6b7124c6b5100e0966ac8b5bc0c6102
kkim6109 <kkim434@wisc.edu>
2021-03-23 20:35:09 -0400

    Homepage and css file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import MoveCalculator from './MoveCalculator.jsx';
-import SignIn from './SignIn.jsx';
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import HomePage from './HomePage.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2> Move Thrower: The Pokemon Move Calculator </h2>
-        <SignIn />
-      </header>
-      <MoveCalculator />
+      <Router>
+          <Switch>
+            <Route exact path = "/" component = {HomePage}/>
+            <Route path = "/Move-Thrower" component = {HomePage}/>
+          </Switch>
+      </Router>
     </div>
   );
 }
+/*
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+	  */
 
 export default App;
