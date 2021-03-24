98bba62145f41da9118f4ec5c8030f2c8ace343a
zzheng26 <56134564+zzheng26@users.noreply.github.com>
2021-03-23 20:49:06 -0500

    Add firebase for user sign up and log in.
    This commit adds user sign up and log in that send data to the firebase.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
 import HomePage from './HomePage.jsx';
+import signInPage from './SignIn.jsx';
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Switch>
-            <Route exact path = "/" component = {HomePage}/>
-            <Route path = "/Move-Thrower" component = {HomePage}/>
+            <Route exact path = "/" component = {signInPage}/>
+            <Route path = "/Move-Thrower" component = {signInPage}/>
+            <Route path = "/Homepage" component = {HomePage}/>
           </Switch>
       </Router>
     </div>
--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,14 +1,35 @@
-//import React from 'react';
+import React from 'react';
+import './App.css';
+import firebase from 'firebase';
+import { Redirect } from 'react-router-dom';
+import createFirebase from './firebase.js';
 
 export class SignIn extends React.Component {
     constructor(props) {
         super(props);
+        this.sendLogIn = this.sendLogIn.bind(this);
+        this.sendSignUp = this.sendSignUp.bind(this);
         this.signInDisplay = this.signInDisplay.bind(this);
         this.loggedInDisplay = this.loggedInDisplay.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.resetErrors = this.resetErrors.bind(this);
+
+        let fbInitialized = false
+		if (this.props.location.state) {
+			fbInitialized = true;
+		}
+
         this.state = {
+            signedIn: false,
             uid: "",
             username: "",
-            password: ""
+            password: "",
+            loginError: "",
+            signUpError: "",
+            preference: "",
+            fireBaseInitialized: fbInitialized,
+            isButtonDisabled: false,
         };
     }
 
@@ -17,18 +38,123 @@ export class SignIn extends React.Component {
         //Call the callback function here
     }
 
+    updateEmail(event) {
+        this.setState({
+            username: event.target.value,
+            isButtonDisabled: false
+        });
+    }
+
+    //Updates the password whenever the user changes it
+    updatePassword(event) {
+        this.setState({
+            password: event.target.value,
+            isButtonDisabled: false
+        });
+    }
+
+    resetErrors() {
+        this.setState({
+            logInError: false,
+            signUpError: false
+        });
+    }
+
+    sendLogIn(){
+        this.resetErrors();
+        //TODO Redirect user to user page, where they can change account info.
+        firebase.auth().signInWithEmailAndPassword(this.state.username, this.state.password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                let userInfo;
+                firebase.database().ref('users/' + user.uid).once('value', (snap)=>{
+                    userInfo = snap.val();
+                    this.setState({
+                        signedIn: true,
+                        uid: user.uid,
+                        preference: user.preference
+                    });
+                });
+                
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage);
+                this.setState({
+                    logInError: errorMessage,
+                    isButtonDisabled: true
+                });
+            });
+    
+    }
+
+    sendSignUp() {
+        this.resetErrors();
+        firebase.auth().createUserWithEmailAndPassword(this.state.username, this.state.password)
+            .then((userCredential) => {
+                // Signed in 
+                var user = userCredential.user;
+                var database = firebase.database();
+                firebase.database().ref('users/' + user.uid).set({
+                    username: this.state.username,
+                });
+                this.setState({
+                    signedIn: true,
+                    uid: user.uid,
+                    preference: ""
+                });
+
+                //TODO Redirect user to finish signing up here
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage);
+                this.setState({
+                    signUpError: errorMessage,
+                    isButtonDisabled: true
+                });
+            });
+    }
+
+    componentDidMount() {
+        if (this.state.fireBaseInitialized === false) {
+            createFirebase();
+            this.setState({
+                fireBaseInitialized: true
+            });
+        }
+    }
+
+
     //function returning display for the sign in display
     signInDisplay() {
-    
+
+        let signUpErrorMessage = this.state.signUpError;
+        const signUpError = signUpErrorMessage !== "" ? <p>{signUpErrorMessage}</p> : null;
+
+        let logInErrorMessage = this.state.logInError;
+        const logInError = logInErrorMessage !== "" ? <p>{logInErrorMessage}</p> : null;
+
+        let signedIn = this.state.signedIn;
+        const signInRedirect = signedIn ? <Redirect push to={{ pathname: "/Homepage", state: { uid: this.state.uid, username: this.state.email, password: this.state.password, 
+             fireBaseInitialized: this.state.fireBaseInitialized }}} /> : null;
+
         return (
             <div classname="signInDisplay">
                 <p>Email: </p>
-                <input type="text" />
+                <input type="text" onChange={this.updateEmail} onBlur={this.updateEmail} />
                 <p>Password: </p>
-                <input type="password" />
+                <input type="password" onChange={this.updatePassword} onBlur={this.updatePassword}/>
                 <div className="signInButtons">
-                    <button>Log In</button>
-                    <button>Sign Up</button>
+                    <button onClick={this.sendLogIn} disabled={this.state.isButtonDisabled}>Log In</button>
+                    <button onClick={this.sendSignUp} disabled={this.state.isButtonDisabled}>Sign Up</button>
+                </div>
+                <div>
+                    {logInError}
+                    {signUpError}
+                    {signInRedirect}
                 </div>
             </div>
         );
@@ -53,7 +179,7 @@ export class SignIn extends React.Component {
         let accountDisplay = curUID === "" ? this.signInDisplay() : this.loggedInDisplay();
         return (
             <div className = "signIn">
-                {accountDisplay}
+                {this.signInDisplay()}
             </div>
         );
     }
