700104628cf5bb7dabbad37adf15ec583e50ca77
glightfoot <47872793+lightfootg@users.noreply.github.com>
2021-04-09 19:19:17 -0500

    updated tests

--- src/Snapshot.test.js
+++ src/Snapshot.test.js
@@ -69,118 +69,148 @@ test('PokemonJSX Snapshot Test', () => {
 //Load Pokemon Test
 test('Load Pokemon Test', () => {
   const wrapper = shallow(<Pokemon />);
-  wrapper.setState({
-    "uid": "",
-    "curPkmn": "Charizard",
-    "baseStats": {
-      "HP": 78,
-      "Atk": 84,
-      "SpAtk": 109,
-      "Def": 78,
-      "SpDef": 85,
-      "Spd": 100
-    },
-    "ivInfo": {
-      "HP": 0,
-      "Atk": 0,
-      "SpAtk": 0,
-      "Def": 0,
-      "SpDef": 0,
-      "Spd": 0
-    },
-    "evInfo": {
-      "HP": 0,
-      "Atk": 0,
-      "SpAtk": 0,
-      "Def": 0,
-      "SpDef": 0,
-      "Spd": 0
-    },
-    "totalStats": {
-      "HP": 253,
-      "Atk": 164,
-      "Def": 153,
-      "SpAtk": 212,
-      "SpDef": 166,
-      "Spd": 195
-    },
-    "level": 95,
-    "nature": "Hardy",
-    "status": "Healthy",
-    "moves": [
-    ],
-    "types": [
-      "Fire",
-      "Flying"
-    ],
-    "pkmnImg": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-    "isAdvanced": true,
-    "readySwap": false,
-    "savedName": "Insert Name",
-    "display": false,
-    "pokemonList": [
-      "Charizard",
-    ]
-  })
+  wrapper.setState(stateToSet)
   expect(wrapper.instance().savePokemon()).toBe(false)
 })
 
-//Type Color Test
-test('Type Color Tests', () => {
-  const wrapper = shallow(<Pokemon />);
-  wrapper.setState({
-    "uid": "",
-    "curPkmn": "Charizard",
-    "baseStats": {
-      "HP": 78,
-      "Atk": 84,
-      "SpAtk": 109,
-      "Def": 78,
-      "SpDef": 85,
-      "Spd": 100
-    },
-    "ivInfo": {
-      "HP": 0,
-      "Atk": 0,
-      "SpAtk": 0,
-      "Def": 0,
-      "SpDef": 0,
-      "Spd": 0
-    },
-    "evInfo": {
-      "HP": 0,
-      "Atk": 0,
-      "SpAtk": 0,
-      "Def": 0,
-      "SpDef": 0,
-      "Spd": 0
-    },
-    "totalStats": {
-      "HP": 253,
-      "Atk": 164,
-      "Def": 153,
-      "SpAtk": 212,
-      "SpDef": 166,
-      "Spd": 195
-    },
-    "level": 95,
-    "nature": "Hardy",
-    "status": "Healthy",
-    "moves": [
-    ],
-    "types": [
-      "Fire",
-      "Flying"
-    ],
-    "pkmnImg": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-    "isAdvanced": true,
-    "readySwap": false,
-    "savedName": "Insert Name",
-    "display": false,
-    "pokemonList": [
-      "Charizard",
-    ]
-  })
-  expect(wrapper.instance().getTypeColor(wrapper.instance().state.types[0])).toBe("#EE8130") &&
-    expect(wrapper.instance().getTypeColor(wrapper.state.types[1])).toBe("#A98FF3")
+//Fire Color Test
+test('Fire Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Fire")).toBe("#EE8130")
+})
+
+//Water Color Test
+test('Water Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Water")).toBe("#6390F0")
+})
+
+//Electric Color Test
+test('Electric Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Electric")).toBe("#F7D02C")
+})
+
+//Grass Color Test
+test('Grass Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Grass")).toBe("#7AC74C")
+})
+
+//Ice Color Test
+test('Ice Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Ice")).toBe("#96D9D6")
+})
+
+//Fighting Color Test
+test('Fighting Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Fighting")).toBe("#C22E28")
+})
+
+//Poison Color Test
+test('Poison Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Poison")).toBe("#A33EA1")
+})
+
+//Ground Color Test
+test('Ground Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Ground")).toBe("#E2BF65")
+})
+
+//Flying Color Test
+test('Flying Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Flying")).toBe("#A98FF3")
+})
+
+//Psychic Color Test
+test('Psychic Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Psychic")).toBe("#F95587")
+})
+
+//Bug Color Test
+test('Bug Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Bug")).toBe("#A6B91A")
+})
+
+//Rock Color Test
+test('Rock Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Rock")).toBe("#B6A136")
+})
+
+//Ghost Color Test
+test('Ghost Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Ghost")).toBe("#735797")
+})
+
+//Dragon Color Test
+test('Dragon Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Dragon")).toBe("#6F35FC")
+})
+
+//Dark Color Test
+test('Dark Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Dark")).toBe("#705746")
+})
+
+//Steel Color Test
+test('Steel Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Steel")).toBe("#B7B7CE")
+})
+
+//Fairy Color Test
+test('Fairy Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Fairy")).toBe("#D685AD")
+})
+
+//Normal Color Test
+test('Normal Color Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet)
+  expect(wrapper.instance().getTypeColor("Normal")).toBe("#A8A77A")
+})
+
+//Update Total Stats Test
+test('updateTotalStats Test', () => {
+  const wrapper = shallow(<Pokemon/>);
+  wrapper.setState(stateToSet);
+  let stats = {
+    HP: 1,
+    Atk: 1,
+    SpAtk: 1,
+    Def: 1,
+    SpDef: 1,
+    Spd: 1
+  }
+  wrapper.instance().updateTotalStats(stats)
+  expect(wrapper.instance().state.totalStats.HP).toBe(1)
 })
